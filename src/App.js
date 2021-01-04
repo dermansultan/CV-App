07593beb93e3b092a74b9a26b4ac6d773c391cdc
dermansultan <abdurrehmansultan4@gmail.com>
2021-01-03 23:44:00 -0500

    form component update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Form from "./Components/form"
 function App() {
-  return <div className="App"></div>;
+  // section for general information like name, email and phone
+  // section for educational experience (school name, title of study, date of study)
+  // add practical experience (company name, position title, main tasks, date from and until you worked for that company)
+  // export?
+  return <div className="App">
+<Form></Form>
+  </div>;
 }
 
 export default App;
