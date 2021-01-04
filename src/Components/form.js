eee8c4bb3b83271fa3cc3ec100a57ed6498457a9
dermansultan <abdurrehmansultan4@gmail.com>
2021-01-04 11:37:07 -0500

    handleChange update + inputs

--- src/Components/form.js
+++ src/Components/form.js
@@ -10,7 +10,7 @@ class Form extends React.Component {
   }
 
   handleChange(event) {
-    this.setState({ value: event.target.value });
+    this.setState({ [event.target.name]: event.target.value });
   }
 
   handleSubmit(event) {
@@ -21,14 +21,102 @@ class Form extends React.Component {
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
+        <div className='information general'>
+        <h2 className='subHeader'>General Information</h2>
+          <label>
+            First Name
+            <input name="fName" type="text" onChange={this.handleChange} />
+          </label>
+          <label>
+            Last Name
+            <input name="lName" type="text" onChange={this.handleChange} />
+          </label>
+          <label>
+            Email
+            <input name="eMail" type="email" onChange={this.handleChange} />
+          </label>
+          <label>
+            Phone Number
+            <input
+              name="pNumb"
+              type="tel"
+              pattern="[0-9]{3}-[0-3]{3}-[0-9]{4}"
+              onChange={this.handleChange}
+            />
+          </label>
+        </div>
+        <div className='information education'>
+        <h2 className='subHeader'>Education</h2>
         <label>
-          Name:
-          <input
-            type="text"
-            value={this.state.value}
-            onChange={this.handleChange}
-          />
-        </label>
+            Institution
+            <input
+              name="school"
+              type="text"
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Program
+            <input
+              name="program"
+              type="text"
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            End Date:
+            <input
+              name="dateStudy"
+              type="date"
+              min="1900-01-01"
+              max="2025-01-01"
+              onChange={this.handleChange}
+            />
+          </label>
+        </div>
+        <div className='information experience'>
+        <h2 className='subHeader'>Experience</h2>
+        <label>
+            Company
+            <input
+              name="company"
+              type="text"
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Position
+            <input
+              name="position"
+              type="text"
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Responsibilities 
+            <input
+              name="tasks"
+              type="text"
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Start Date 
+            <input
+              name="startDate"
+              type="date"
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            End Date  
+            <input
+              name="endDate"
+              type="date"
+              onChange={this.handleChange}
+            />
+          </label>
+        </div>
         <input type="submit" value="Submit" />
       </form>
     );
